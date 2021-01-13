f64db291bc3609b67a0bc6e908031f654d993670
adnanq2004 <aquayyum20@stuy.edu>
2021-01-12 19:32:20 -0500

    may have made it work with negative. to be tested

--- Radix.java
+++ Radix.java
@@ -38,7 +38,7 @@ public class Radix{
 
 	public static void radixSortSimple(SortableLinkedList data) {
 	
-		SortableLinkedList bucket0 = new SortableLinkedList();
+		/*SortableLinkedList bucket0 = new SortableLinkedList();
 		SortableLinkedList bucket1 = new SortableLinkedList();
 		SortableLinkedList bucket2 = new SortableLinkedList();
 		SortableLinkedList bucket3 = new SortableLinkedList();
@@ -71,8 +71,190 @@ public class Radix{
 			}
 			main.extend(data);
 			merge(data, buckets);
+		}*/
+
+		data = thing(data);
+	}
+
+	private static SortableLinkedList thing(SortableLinkedList data) {
+
+		
+                /*SortableLinkedList bucket0 = new SortableLinkedList();
+                SortableLinkedList bucket1 = new SortableLinkedList();
+                SortableLinkedList bucket2 = new SortableLinkedList();
+                SortableLinkedList bucket3 = new SortableLinkedList();
+                SortableLinkedList bucket4 = new SortableLinkedList();
+                SortableLinkedList bucket5 = new SortableLinkedList();
+                SortableLinkedList bucket6 = new SortableLinkedList();
+                SortableLinkedList bucket7 = new SortableLinkedList();
+                SortableLinkedList bucket8 = new SortableLinkedList();
+                SortableLinkedList bucket9 = new SortableLinkedList();
+
+                SortableLinkedList[] buckets= new SortableLinkedList[10];
+                buckets[0] = bucket0;
+                buckets[1] = bucket1;
+                buckets[2] = bucket2;
+                buckets[3] = bucket3;
+                buckets[4] = bucket4;
+                buckets[5] = bucket5;
+                buckets[6] = bucket6;
+                buckets[7] = bucket7;
+                buckets[8] = bucket8;
+                buckets[9] = bucket9;
+
+                SortableLinkedList main = new SortableLinkedList();
+
+                int max = greatestdig(data);
+                for(int i = 0; i < max; i++) {
+                        for (int j = 0; j < data.size(); j++) {
+                                int column = nth(data.get(j), i);
+                                buckets[column].add(data.get(j));
+                        }
+                        main.extend(data);
+                        merge(data, buckets);
+                }*/
+		SortableLinkedList bucket0 = new SortableLinkedList();
+                SortableLinkedList bucket1 = new SortableLinkedList();
+                SortableLinkedList bucket2 = new SortableLinkedList();
+                SortableLinkedList bucket3 = new SortableLinkedList();
+                SortableLinkedList bucket4 = new SortableLinkedList();
+                SortableLinkedList bucket5 = new SortableLinkedList();
+                SortableLinkedList bucket6 = new SortableLinkedList();
+                SortableLinkedList bucket7 = new SortableLinkedList();
+                SortableLinkedList bucket8 = new SortableLinkedList();
+                SortableLinkedList bucket9 = new SortableLinkedList();
+
+                SortableLinkedList[] buckets= new SortableLinkedList[10];
+                buckets[0] = bucket0;
+                buckets[1] = bucket1;
+                buckets[2] = bucket2;
+                buckets[3] = bucket3;
+                buckets[4] = bucket4;
+                buckets[5] = bucket5;
+                buckets[6] = bucket6;
+                buckets[7] = bucket7;
+                buckets[8] = bucket8;
+                buckets[9] = bucket9;
+
+                SortableLinkedList main = new SortableLinkedList();
+
+                int max = greatestdig(data);
+                for(int i = 0; i < max; i++) {
+                        for (int j = 0; j < data.size(); j++) {
+                                int column = nth(data.get(j), i);
+                                buckets[column].add(data.get(j));
+                        }
+                        main.extend(data);
+                        merge(data,buckets);
+                        for (int n = 0; n < buckets.length; n++) {
+                                main.extend(buckets[n]);
+			}
 		}
-		System.out.println(data);
+		return data;
+	}
+
+	public static void radixSort(SortableLinkedList data) {
+		
+		/*SortableLinkedList bucket0 = new SortableLinkedList();
+                SortableLinkedList bucket1 = new SortableLinkedList();
+                SortableLinkedList bucket2 = new SortableLinkedList();
+                SortableLinkedList bucket3 = new SortableLinkedList();
+                SortableLinkedList bucket4 = new SortableLinkedList();
+                SortableLinkedList bucket5 = new SortableLinkedList();
+                SortableLinkedList bucket6 = new SortableLinkedList();
+                SortableLinkedList bucket7 = new SortableLinkedList();
+                SortableLinkedList bucket8 = new SortableLinkedList();
+                SortableLinkedList bucket9 = new SortableLinkedList();
+
+                SortableLinkedList[] buckets= new SortableLinkedList[10];
+                buckets[0] = bucket0;
+                buckets[1] = bucket1;
+                buckets[2] = bucket2;
+                buckets[3] = bucket3;
+                buckets[4] = bucket4;
+                buckets[5] = bucket5;
+                buckets[6] = bucket6;
+                buckets[7] = bucket7;
+                buckets[8] = bucket8;
+                buckets[9] = bucket9;
+
+                SortableLinkedList main = new SortableLinkedList();
+
+                int max = greatestdig(data);
+                for(int i = 0; i < max; i++) {
+                        for (int j = 0; j < data.size(); j++) {
+                                int column = nth(data.get(j), i);
+                                buckets[column].add(data.get(j));
+                        }
+                        main.extend(data);
+			merge(data,buckets)
+			for (int n = 0; n < buckets.length; n++) 
+				main.extend(buckets[n]);
+			{
+                }*/
+
+		radixSortSimple(data);
+
+		
+
+		SortableLinkedList bucketnegative = new SortableLinkedList();
+		SortableLinkedList bucketnonnegative = new SortableLinkedList();
+		SortableLinkedList[] buckets2 = new SortableLinkedList[2];
+		buckets2[0] = bucketnegative;
+		buckets2[1] = bucketnonnegative;
+
+		for (int i = 0; i < data.size(); i++) {
+			if (data.get(i) < 0) {
+				buckets2[0].add(data.get(i));
+			}
+			else {
+				buckets2[1].add(data.get(i));
+			}
+		}
+
+		SortableLinkedList bucket0 = new SortableLinkedList();
+                SortableLinkedList bucket1 = new SortableLinkedList();
+                SortableLinkedList bucket2 = new SortableLinkedList();
+                SortableLinkedList bucket3 = new SortableLinkedList();
+                SortableLinkedList bucket4 = new SortableLinkedList();
+                SortableLinkedList bucket5 = new SortableLinkedList();
+                SortableLinkedList bucket6 = new SortableLinkedList();
+                SortableLinkedList bucket7 = new SortableLinkedList();
+                SortableLinkedList bucket8 = new SortableLinkedList();
+                SortableLinkedList bucket9 = new SortableLinkedList();
+
+                SortableLinkedList[] buckets= new SortableLinkedList[10];
+
+		buckets[0] = bucket9;
+		buckets[1] = bucket8;
+		buckets[2] = bucket7;
+		buckets[3] = bucket6;
+		buckets[4] = bucket5;
+		buckets[5] = bucket4;
+		buckets[6] = bucket3;
+		buckets[7] = bucket2;
+		buckets[8] = bucket1;
+		buckets[9] = bucket0;
+		
+		SortableLinkedList trash = new SortableLinkedList();
+
+		int max2 = greatestdig(buckets2[0]);
+		for (int i = 0; i < max2; i++) {
+			for (int j = 0; j < buckets2[0].size(); j++) {
+				int temp = Math.abs(buckets2[0].get(j));
+				int column = nth(temp, i);
+				buckets[column].add(buckets2[0].get(j));
+			}
+			trash.extend(buckets2[0]);
+			merge(buckets2[0], buckets);
+			for (int n = 0; n < buckets.length; n++) {
+				trash.extend(buckets[n]);
+			}
+		}
+
+		trash.extend(data);
+		merge(data, buckets2);
+
 	}
 
 	private static int greatestdig(SortableLinkedList list) {
